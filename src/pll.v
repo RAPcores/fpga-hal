20b0e9a62dde3a3ef86d8c4ba56e8dbb30c1d44c
Steve Kelly <kd2cca@gmail.com>
2021-04-23 17:45:08 -0400

    fixup function for constant propogation

--- src/pll.v
+++ src/pll.v
@@ -7,8 +7,9 @@ module pll #(
 ) (
     input wire pllin,
     output wire pllout,
-    output wire locked
-    //output wire found_something_w
+    output wire locked,
+    output wire found_something_w,
+    output wire [31:0] fout
 );
 
 
@@ -19,19 +20,19 @@ module pll #(
             input integer f_pllin;
             input integer f_pllout;
             input simple_feedback;
-            input integer best_fout = 0;
-            input integer best_divr = 0;
-            input integer best_divf = 0;
-            input integer best_divq = 0;
-            input integer found_something = 0;
-            input integer f_pfd = 0;
-            input integer f_vco = 0;
-            input integer fout = 0;
-            input integer choice_err = 0;
-            input integer best_err = 0;
-            input integer divr = 0;
-            input integer divf = 0;
-            input integer divq = 0;
+            integer best_fout;
+            integer best_divr;
+            integer best_divf;
+            integer best_divq;
+            integer found_something;
+            integer f_pfd;
+            integer f_vco;
+            integer fout;
+            integer choice_err;
+            integer best_err;
+            integer divr;
+            integer divf;
+            integer divq;
 
             // The documentation in the iCE40 PLL Usage Guide incorrectly lists the
             // maximum value of DIVF as 63, when it is only limited to 63 when using
@@ -47,6 +48,10 @@ module pll #(
             for(divr = 0; divr < 16; divr=divr+1) begin
                 f_pfd = (f_pllin / (divr + 1));
                 if (!(f_pfd < 10*1e6 || f_pfd > 133*1e6)) for (divf = 0; divf <= divf_max; divf=divf+1) begin
+                    if (divr == 0 && divf == 0) begin
+                        best_fout = 0;
+                        found_something = 0;
+                    end
                     if (simple_feedback) begin
 
                         f_vco = f_pfd * (divf + 1);
@@ -56,6 +61,12 @@ module pll #(
                             choice_err = fout > f_pllout ? fout - f_pllout : f_pllout - fout;
                             best_err = best_fout > f_pllout ? best_fout - f_pllout : f_pllout - best_fout;
                             if (choice_err < best_err || !found_something) begin
+                                best_fout = fout;
+                                best_divr = divr;
+                                best_divf = divf;
+                                best_divq = divq;
+                                found_something = 1;
+
                                 ice40_pll_comp[31:0] = fout;
                                 ice40_pll_comp[63:32] = divr;
                                 ice40_pll_comp[95:64] = divf;
@@ -84,11 +95,13 @@ module pll #(
             end
         endfunction
 
-    localparam ice40_result = ice40_pll_comp(f_pllin, f_pllout, simple_feedback);
+        localparam ice40_result = ice40_pll_comp(f_pllin, f_pllout, simple_feedback);
+        assign found_something_w = ice40_result[129];
+        assign fout = ice40_result[31:0];
 
-    initial begin
-        $display(ice40_result);
-    end
+        initial begin
+            $display(ice40_result);
+        end
 
     end
 
